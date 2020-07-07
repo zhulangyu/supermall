a654dec941dde36e65850d614bdb070c6125a7dd
zhulangyu <646618120@qq.com>
2020-07-07 10:39:11 +0800

    项目文件

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,16 +1,45 @@
 <template>
- <nav-bar class="home-nav">
-   <div slot="center">购物街</div>
- </nav-bar>
+ <div>
+   <nav-bar class="home-nav"> <div slot="center">购 物 街</div></nav-bar>
+   <home-swiper :banners="banners" />
+   <recommend-view :recommends="recommends" />
+ </div>
 </template>
 
 <script>
-import NavBar from "components/common/navbar/NavBar"
+import NavBar from "components/common/navbar/NavBar";
+import HomeSwiper from "./chindcomponents/homeSwiper"
+import RecommendView from "./chindcomponents/recommendviews"
+
+
+import {getHomeMultidata} from "network/home"
+
   export default {
     name: "Home",
     components:{
       NavBar,
-    }
+      HomeSwiper,
+      RecommendView,
+    },
+    data() {
+      return {
+        banners:[],
+        recommends:[],
+        // result:null
+      
+        
+      }
+    },
+    
+    created() {
+      getHomeMultidata().then(res => {
+        // console.log(res)
+        this.banners = res.data.data.banner.list;
+        this.recommends = res.data.data.recommend.list;
+        // this.result=res
+        
+      }) 
+    },  
   }
 </script>
 
--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  configureWebpack: {
-    resolve: {
-      alias: {
-        'assets': '@/assets',
-        'common': '@/common',
-        'components': '@/components',
-        'network': '@/network',
-        'views': '@/views',
-      }
+    lintOnSave: false,
+    configureWebpack: {
+        resolve: {
+            alias: {
+                'assets': '@/assets',
+                'common': '@/common',
+                'components': '@/components',
+                'network': '@/network',
+                'views': '@/views',
+            }
+        }
     }
-  }
 }
-
